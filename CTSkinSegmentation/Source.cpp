929f23395b569694268643e0e26e1eff1ac26c64
unknown <hhe4488@hanmail.net>
2020-07-08 15:06:59 +0900

    modify comments_iostream, namespace

--- CTSkinSegmentation/Source.cpp
+++ CTSkinSegmentation/Source.cpp
@@ -1,10 +1,13 @@
 //헤더파일들 include
 #include <opencv2/opencv.hpp> 
 #include <opencv2/highgui/highgui.hpp>
-#include <iostream>
+#include <iostream> //cout, cin 함수 사용하기 위해 (iostream 클래스 내의 객체)
 
+//namespace : 식별자(변수, 함수명 등)의 소속 공간(name space), 같은 이름이어도 다르게 구분 가능하게 해줌(충돌 방지), 접두어 생략 가능
+//*'네임스페이스' 'std'에 cout, cin같은 함수가 소속되어있고, 'iostream'이라는 '헤더파일'에 이 내용들이 작성되어 있음
+using namespace std; //STL(라이브러리): std 네임스페이스 소속
 using namespace cv;
-using namespace std;
+
 
 int main(){
 	Mat img = imread("test.png");
