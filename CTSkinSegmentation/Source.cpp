7cd3b8411fed42bd45829991c64a22d36e7552a8
unknown <hhe4488@hanmail.net>
2020-07-10 15:33:25 +0900

    imread with Absolute Path

--- CTSkinSegmentation/Source.cpp
+++ CTSkinSegmentation/Source.cpp
@@ -2,6 +2,7 @@
 #include <opencv2/opencv.hpp> 
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream> //cout, cin 함수 사용하기 위해 (iostream 클래스 내의 객체)
+#include "stdafx.h" //precompiled header (미리 컴파일 시켜주는 역할, 전체적으로 빌드 시간 감소)
 
 //namespace : 식별자(변수, 함수명 등)의 소속 공간(name space), 같은 이름이어도 다르게 구분 가능하게 해줌(충돌 방지), 접두어 생략 가능
 //*'네임스페이스' 'std'에 cout, cin같은 함수가 소속되어있고, 'iostream'이라는 '헤더파일'에 이 내용들이 작성되어 있음
@@ -10,8 +11,8 @@ using namespace cv;
 
 
 int main(){
-	Mat img = imread("test.png");
-	imshow("test", img);
+	Mat img = imread("C:\\Users\\Ryu\\Desktop\\200707_CTSkinSegmentation_SRC\\img_default\\Breast0002.png");
+	imshow("B0002", img);
 	waitKey(0);
 	destroyAllWindows();
 
