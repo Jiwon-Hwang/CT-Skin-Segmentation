1382b4a8217a9bc7efbabd32c42bac686a06e51b
unknown <hhe4488@hanmail.net>
2020-07-29 12:02:35 +0900

    floodfill_seed coordinate...

--- CTSkinSegmentation/Source.cpp
+++ CTSkinSegmentation/Source.cpp
@@ -9,26 +9,35 @@
 using namespace std; //STL(라이브러리): std 네임스페이스 소속
 using namespace cv;
 
-
 int main(){
 	//0. Absolute Path setting ==> 추후 경로값 저장해두기
-	Mat ori = imread("C:\\Users\\Ryu\\Desktop\\200707_CTSkinSegmentation_SRC\\img_default\\Breast0002.png", 0);
+	Mat ori = imread("C:\\Users\\Ryu\\Desktop\\200707_CTSkinSegmentation_SRC\\img_default\\Breast0067.png", 0);
 	Mat img_copy = ori.clone();
 
 	//1. Bilateral Filtering (noise filtering)
 	Mat filtered;
 	bilateralFilter(img_copy, filtered, -1, 15, 15); //(src, dst, d(필터링 수행할 지름), sigmaColor(색 공간), sigmaSpace(거리 공간))
-	imwrite("C:\\Users\\Ryu\\Desktop\\200707_CTSkinSegmentation_SRC\\img_result\\Breast0002_filtered.png", filtered);
+	imwrite("C:\\Users\\Ryu\\Desktop\\200707_CTSkinSegmentation_SRC\\img_result\\Breast0067_filtered.png", filtered);
 
 	//2. Otsu's Thresholding
 	Mat otsu;
 	threshold(img_copy, otsu, 0, 255, THRESH_BINARY|THRESH_OTSU); 
-	imwrite("C:\\Users\\Ryu\\Desktop\\200707_CTSkinSegmentation_SRC\\img_result\\Breast0002_otsu.png", otsu);
+	imwrite("C:\\Users\\Ryu\\Desktop\\200707_CTSkinSegmentation_SRC\\img_result\\Breast0067_otsu.png", otsu);
 
+	/*
 	//3. watershed : marker구하기(with. Erosion, Dilation, connectedComponents) -> watershed
 	//Mat markers = Mat::zeros(img_copy.size(), CV_32S);
 	watershed(img_copy, markers); //3채널만 가능...(error) ==> 3채널로 변환 or SRG로 수행
-	imwrite("C:\\Users\\Ryu\\Desktop\\200707_CTSkinSegmentation_SRC\\img_result\\Breast0002_watershed.png", markers);
+	imwrite("C:\\Users\\Ryu\\Desktop\\200707_CTSkinSegmentation_SRC\\img_result\\Breast0067_watershed.png", markers);
+	*/
+
+	//3. Floodfil
+	Mat floodfill = otsu.clone();
+	floodFill(floodfill, Point(0,16), Scalar(0)); //from point (0,0)
+	imwrite("C:\\Users\\Ryu\\Desktop\\200707_CTSkinSegmentation_SRC\\img_result\\Breast0067_floodfill.png", floodfill);
+
+	
+
 
 	return 0;
 }
